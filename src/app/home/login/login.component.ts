28b76164872e27643793e30bcb40f4f5025ef1c6
thiagofrancisquete <thiago.francisquete@gmail.com>
2021-12-27 16:50:45 -0300

    metodo de login

--- src/app/home/login/login.component.ts
+++ src/app/home/login/login.component.ts
@@ -1,15 +1,29 @@
+import { AutenticacaoService } from './../../autenticacao/autenticacao.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  usuario: string = '';
+  senha: string = '';
 
-  constructor() { }
+  constructor(private authService: AutenticacaoService, private router: Router) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  login() {
+    this.authService.autenticar(this.usuario, this.senha).subscribe(
+      () => {
+        this.router.navigate(['animais'])
+      },
+      (error) => {
+        alert('Usuario ou senha inválida');
+        console.log(error);
+      }
+    );
   }
-
 }
