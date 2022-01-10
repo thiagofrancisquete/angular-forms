ababa2b022496fc8cc7c9a6cf302b44c9000c590
thiagofrancisquete <thiago.francisquete@gmail.com>
2022-01-10 15:50:31 -0300

    serviço criar usuario

---
+++ src/app/home/novo-usuario/novo-usuario.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { NovoUsuario } from './novo-usuario';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NovoUsuarioService {
+
+  constructor(private http: HttpClient) { }
+
+  cadastrarNovoUsuario(novoUsuario: NovoUsuario) {
+    return this.http.post('http://localhost:3000/user/signup', novoUsuario)
+  }
+
+  userExists(nomeUsuario: NovoUsuario) {
+    return this.http.get(`http://localhost:3000/user/exists/${nomeUsuario}`)
+  }
+}
